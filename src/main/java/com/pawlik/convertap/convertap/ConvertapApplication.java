9427d92c5be0173956ee4709c61d637b2f3a5ea5
Jarosław Pawlik <JAROSLAW.pawlik@yahoo.com>
2024-11-15 17:56:05 +0100

    Initial commit

---
+++ src/main/java/com/pawlik/convertap/convertap/ConvertapApplication.java
@@ -0,0 +1,13 @@
+package com.pawlik.convertap.convertap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConvertapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConvertapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawlik/convertap/convertap/ConvertapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawlik.convertap.convertap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConvertapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
